2a459f424eb72f1bc80b9ada39b97059edb61f1a
NIKolayM94 <49314579+NIKolayM94@users.noreply.github.com>
2020-11-01 22:58:01 +0300

    оптимизация скрипта

--- changeTabs.js
+++ changeTabs.js
@@ -1,21 +1,21 @@
 let nav = document.querySelector(".page-nav");
 let tabsNav = nav.querySelectorAll("li");
 let tabs = document.querySelectorAll(".tab");
-let selectedTab = '';
+let selectedTab = tabsNav[0].dataset.nav;
+
+console.log(tabsNav);
+
+window.onload = function () {
+	tabsNav[0].classList.add("selected");
+	hiddenClass();
+}
 
 for (let tabNav of tabsNav) {
 	tabNav.onclick = function () {
 		removeClass(tabsNav, "selected");
 		tabNav.classList.add("selected");
 		selectedTab = tabNav.dataset.nav;
-		for (let tab of tabs) {
-			if (tab.classList.contains(selectedTab)) {
-				tab.classList.remove("hidden");
-			}
-			else if (!tab.classList.contains("hidden")) {
-				tab.classList.add("hidden");
-			}
-		}
+		hiddenClass();
 	}
 }
 
@@ -25,4 +25,15 @@ let removeClass = function (array, itemClass) {
 				array[i].classList.remove(itemClass);
 			}
 	};
+}
+
+let hiddenClass = function () {
+	for (let tab of tabs) {
+		if (tab.classList.contains(selectedTab)) {
+			tab.classList.remove("hidden");
+		}
+		else if (!tab.classList.contains("hidden")) {
+			tab.classList.add("hidden");
+		}
+	}
 }
